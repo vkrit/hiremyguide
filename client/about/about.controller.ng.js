21e1f6c86848030b3a06a192a467bec625213679
Veerasak Kritsanapraphan <veerasak.kr@172.18.40.66>
2015-10-10 16:27:51 +0700

    Initial commit

---
+++ client/about/about.controller.ng.js
@@ -0,0 +1,6 @@
+'use strict';
+
+angular.module('hiremyguideApp')
+.controller('AboutCtrl', function($scope) {
+  $scope.viewName = 'About';
+});
---
+++ client/lib/app.ng.js
@@ -0,0 +1,16 @@
+angular.module('hiremyguideApp', [
+  'angular-meteor',
+  'ui.router',
+  'ui.bootstrap',
+  'angularUtils.directives.dirPagination'
+]);
+
+onReady = function() {
+  angular.bootstrap(document, ['hiremyguideApp']);
+};
+  
+if(Meteor.isCordova) {
+  angular.element(document).on('deviceready', onReady);
+} else {
+  angular.element(document).ready(onReady);
+}
